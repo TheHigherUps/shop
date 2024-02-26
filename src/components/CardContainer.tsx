a721e681240aa6ce93792c7b57bb12a3fbd253d3
TheHigherUps <classified@thehigherups.org>
2024-02-26 14:39:38 -0500

    style: changed CardContainer width

--- src/components/CardContainer.tsx
+++ src/components/CardContainer.tsx
@@ -4,7 +4,7 @@ export default function CardContainer({
     children: React.ReactNode
 }) {
     return (
-        <div className="w-full md:w-[727px] mx-auto min-h-[350px] bg-white text-black rounded-xl py-4">
+        <div className="w-full md:w-[787px] mx-auto min-h-[350px] bg-white text-black rounded-xl py-4">
             {children}
         </div>
     )
