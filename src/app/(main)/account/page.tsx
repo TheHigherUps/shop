3ad1b61b350685d3895c8c48e66a7edf47d11ea7
TheHigherUps <classified@thehigherups.org>
2024-02-27 08:58:56 -0500

    feat: changed Banner to use props as message

--- src/app/(main)/account/page.tsx
+++ src/app/(main)/account/page.tsx
@@ -1,4 +1,5 @@
 "use client"
+import Banner from "@/components/Banner"
 import { useAuth } from "@/contexts/AuthContext"
 import { LogOut } from "lucide-react"
 import Link from "next/link"
@@ -33,66 +34,78 @@ const Login = () => {
         await login(formData.email, formData.password)
     }
     return (
-        <main className="grid place-items-center min-h-[calc(100vh-3rem)]">
-            <div className="flex flex-col gap-4 px-12 py-4 rounded-xl bg-white text-black mt-12 items-center">
-                <div className="text-center">
-                    <div className="text-5xl font-bold">
-                        Login To Your Account
+        <>
+            <Banner>Account Sign-in Temporarily Disabled</Banner>
+            <main className="grid place-items-center min-h-[calc(100vh-6rem)]">
+                <div className="flex flex-col gap-4 px-12 py-4 rounded-xl bg-white text-black mt-12 items-center">
+                    <div className="text-center">
+                        <div className="text-5xl font-bold">
+                            Login To Your Account
+                        </div>
+                    </div>
+                    <form
+                        onSubmit={handleSubmit}
+                        className="flex flex-col gap-3 text-center"
+                    >
+                        <label htmlFor="email" className="text-3xl font-bold">
+                            Enter Email Address
+                        </label>
+                        <input
+                            value={formData.email}
+                            onChange={(e) =>
+                                setFormData({
+                                    ...formData,
+                                    email: e.target.value,
+                                })
+                            }
+                            id="email"
+                            type="email"
+                            className="rounded bg-[#cccccc] border border-black w-full text-xl"
+                        />
+                        <label
+                            htmlFor="password"
+                            className="text-3xl font-bold"
+                        >
+                            Enter Password
+                        </label>
+                        <input
+                            value={formData.password}
+                            onChange={(e) => {
+                                setFormData({
+                                    ...formData,
+                                    password: e.target.value,
+                                })
+                            }}
+                            id="password"
+                            type="password"
+                            className="rounded bg-[#cccccc] border border-black w-full text-xl"
+                        />
+                        <button
+                            type="submit"
+                            className="rounded mt-12 w-fit px-1 mx-auto bg-[#cccccc] border border-black  text-xl font-bold"
+                        >
+                            Login to Account
+                        </button>
+                    </form>
+                    <div className="flex flex-col items-center">
+                        <span className="text-[#bcbcbc]">
+                            Don&apos;t Have An Account?{" "}
+                            <Link
+                                className="underline"
+                                href={"/create-account"}
+                            >
+                                Click Here To Make One
+                            </Link>
+                        </span>
+                        <span className="text-[#bcbcbc]">
+                            Forgot Your Password?{" "}
+                            <Link className="underline" href="/password-reset">
+                                Click Here To Reset It
+                            </Link>
+                        </span>
                     </div>
                 </div>
-                <form
-                    onSubmit={handleSubmit}
-                    className="flex flex-col gap-3 text-center"
-                >
-                    <label htmlFor="email" className="text-3xl font-bold">
-                        Enter Email Address
-                    </label>
-                    <input
-                        value={formData.email}
-                        onChange={(e) =>
-                            setFormData({ ...formData, email: e.target.value })
-                        }
-                        id="email"
-                        type="email"
-                        className="rounded bg-[#cccccc] border border-black w-full text-xl"
-                    />
-                    <label htmlFor="password" className="text-3xl font-bold">
-                        Enter Password
-                    </label>
-                    <input
-                        value={formData.password}
-                        onChange={(e) => {
-                            setFormData({
-                                ...formData,
-                                password: e.target.value,
-                            })
-                        }}
-                        id="password"
-                        type="password"
-                        className="rounded bg-[#cccccc] border border-black w-full text-xl"
-                    />
-                    <button
-                        type="submit"
-                        className="rounded mt-12 w-fit px-1 mx-auto bg-[#cccccc] border border-black  text-xl font-bold"
-                    >
-                        Login to Account
-                    </button>
-                </form>
-                <div className="flex flex-col items-center">
-                    <span className="text-[#bcbcbc]">
-                        Don&apos;t Have An Account?{" "}
-                        <Link className="underline" href={"/create-account"}>
-                            Click Here To Make One
-                        </Link>
-                    </span>
-                    <span className="text-[#bcbcbc]">
-                        Forgot Your Password?{" "}
-                        <Link className="underline" href="/password-reset">
-                            Click Here To Reset It
-                        </Link>
-                    </span>
-                </div>
-            </div>
-        </main>
+            </main>
+        </>
     )
 }
--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,7 @@
-export default function Banner() {
+export default function Banner({ children }: { children: React.ReactNode }) {
     return (
         <div className="w-full h-12 text-3xl font-bold grid place-items-center bg-[#d4d421] text-black">
-            <div>Teaksdjkasjdkasdjkajsdajd MESSAGE</div>
+            <div>{children}</div>
         </div>
     )
 }
