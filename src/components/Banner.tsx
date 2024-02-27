2744508005a6b5fffd3f00f297308f3c6602a740
TheHigherUps <classified@thehigherups.org>
2024-02-27 10:26:11 -0500

    style: fixed banner on smaller screens changed banner text to center

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,6 @@
 export default function Banner({ children }: { children: React.ReactNode }) {
     return (
-        <div className="w-full h-12 text-3xl font-bold grid place-items-center bg-[#d4d421] text-black">
+        <div className="w-full min-h-12 text-center text-3xl font-bold grid place-items-center bg-[#d4d421] text-black">
             <div>{children}</div>
         </div>
     )
