716fcc61f864f7c5989fbca6e0266e68964aa5ba
TheHigherUps <classified@thehigherups.org>
2024-02-27 09:20:41 -0500

    style: removed time display on smaller screens

--- src/components/TimeDisplay.tsx
+++ src/components/TimeDisplay.tsx
@@ -14,9 +14,8 @@ export default function TimeDisplay() {
             clearInterval(x)
         }
     }, [time])
-
     return (
-        <aside className="sticky top-8 w-[200px] px-8 text-center bg-white text-black m-5 rounded-full">
+        <aside className="hidden xl:block sticky top-8 w-[200px] px-8 text-center bg-white text-black m-5 rounded-full">
             <div className="text-3xl">Local Time</div>
             <span className="text-2xl">{time}</span>
         </aside>
