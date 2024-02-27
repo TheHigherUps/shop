98df406158502c0efdd847b0d177aae415a4851f
TheHigherUps <classified@thehigherups.org>
2024-02-27 08:11:54 -0500

    style: added coloring to shop cart when clicked

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,11 +1,20 @@
 "use client"
 import { ShoppingCart } from "lucide-react"
+import { useState } from "react"
 
 export default function Hero() {
+    const [clicked, setClicked] = useState(false)
     function playHonk() {
+        if (clicked) {
+            return
+        }
+        setClicked(true)
         const audio = new Audio("/assets/sounds/oos-error.mp3")
         audio.play()
         // alert("HONK!")
+        setTimeout(() => {
+            setClicked(false)
+        }, 500)
     }
     return (
         <div className="photoHeader background">
@@ -33,8 +42,9 @@ export default function Hero() {
                 TheHigherUps Shop{" "}
                 <ShoppingCart
                     onClick={playHonk}
-                    className="align-middle inline"
+                    className=" align-middle inline transition-colors"
                     fill="1"
+                    stroke={clicked ? "#dad" : "white"}
                     size={30}
                 />
             </h1>
