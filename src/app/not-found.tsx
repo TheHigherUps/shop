5aac2659a445ef58f5851ce914b7c0227be738fe
TheHigherUps <classified@thehigherups.org>
2024-02-29 08:24:32 -0500

    style: updated not-found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,49 @@
 "use client"
-import Image from "next/image";
-import Link from "next/link";
-import { useEffect } from "react";
+import Header from "@/components/Header"
+import { cn } from "@/lib/utils"
+import { Frown } from "lucide-react"
+import { Dongle, Nabla } from "next/font/google"
+import Image from "next/image"
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+import { useEffect } from "react"
+
+const font = Nabla({ subsets: ["latin"] })
+const dongle = Dongle({ subsets: ["latin"], weight: "300" })
 
 export default function NotFound() {
-  useEffect(() => {
-
-  },[])
-  return (
-   <div className="text-black">
-    error 404
-    <Image className="[animation-duration:1500ms] animate-spin " src="/assets/images/sad-face.png" alt="Sad Face" width={150} height={150} />
-   </div>
-  )
+    const pathname = usePathname()
+    return (
+        <main
+            className={
+                font.className +
+                " min-h-screen grid place-items-center bg-[#2d6c9c]"
+            }
+        >
+            <div className="flex flex-col items-center gap-12 text-4xl">
+                <div>
+                    Oops! You Messed Up <Frown className="inline" size={36} />
+                </div>
+                <div>You&apos;ve Encountered A Wild 404 Error</div>
+                <div>{pathname} Doesn&apos;t Exist</div>
+                <div className="relative h-32 w-32 ">
+                    <Image
+                        className="animate-spin"
+                        src="/assets/images/sad-face.png"
+                        alt={""}
+                        fill
+                    />
+                </div>
+                <Link
+                    className={cn(
+                        dongle.className,
+                        "rounded-xl bg-slate-400 text-black px-3 py-2 border border-black"
+                    )}
+                    href="/"
+                >
+                    Return Home
+                </Link>
+            </div>
+        </main>
+    )
 }
