a58c48d54caad70f3c5155055566fcb904eeaf64
TheHigherUps <classified@thehigherups.org>
2024-03-04 16:47:42 -0500

    style: updated MovingPopup

--- src/components/MovingPopup.tsx
+++ src/components/MovingPopup.tsx
@@ -21,18 +21,21 @@ export default function MovingPopup() {
         return null
     }
     return (
-        <div className="fixed bottom-12 left-1/2 -translate-x-1/2 -tran w-3/4 min-h-16  text-black z-[9999] ">
-            <div className="w-full h-full px-2 relative beans bg-[#d8bfd8]">
-                <div className="text-3xl">
+        <div className="fixed bottom-12 left-1/2 -translate-x-1/2 w-3/4 text-black z-[9999] ">
+            <div className="w-full h-full px-2 relative beans bg-[#d8bfd8] flex flex-col p-2 text-2xl">
+                <div className="font-extrabold">
                     WOAH! CHECK OUT THIS LIMITED TIME OFFER
                 </div>
-                <p className="text-2xl">
+                <p className="font-extrabold">
                     Check Out This Limted Time Offer By Using Code
                     &apos;SecretBeansCB&apos; For An Extra Can Of Assualt Beans
                     FOR FREE!
                 </p>
                 <button onClick={() => closeBanner()}>
-                    <X className="absolute top-2 right-2 hover:stroke-red-500 transition-colors" />
+                    <X
+                        size={18}
+                        className="absolute top-2 right-2 hover:stroke-red-500 transition-colors"
+                    />
                 </button>
             </div>
         </div>
