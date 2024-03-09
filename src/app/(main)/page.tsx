bf75675c31d4402506acc6f07e58f8fdf81b49f8
TheHigherUps <classified@thehigherups.org>
2024-03-09 03:48:39 -0500

    refactor: re-added suspense boundary to home page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,5 +1,10 @@
-import Home from "@/app/(main)/Home";
+import Home from "@/app/(main)/Home"
+import { Suspense } from "react"
 
 export default function Page() {
-    return <Home />
+    return (
+        <Suspense>
+            <Home />
+        </Suspense>
+    )
 }
