17fda7f0ae6ad5ad4b3d960d23482eb5b32897d3
TheHigherUps <classified@thehigherups.org>
2024-03-14 10:50:05 -0400

    feat: added banner button

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,4 +1,6 @@
 import { cn } from "@/lib/utils"
+import Link from "next/link"
+import React from "react"
 
 export default function Banner({
     className,
@@ -11,10 +13,23 @@ export default function Banner({
         <div
             className={cn(
                 "w-full min-h-12 text-center text-3xl font-bold grid place-items-center bg-[#d4d421] text-black",
-                className
+                className,
             )}
         >
             <div>{children}</div>
         </div>
+    )
+}
+export const BannerButton = ({
+    href,
+    children,
+}: {
+    href: string
+    children: React.ReactNode
+}) => {
+    return (
+        <Link className="ml-4 border border-black rounded-xl px-2" href={href}>
+            {children}
+        </Link>
     )
 }
